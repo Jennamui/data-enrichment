a304f1db4d318cf5002e1ea8d54754e535b29032
Jenna Mui <Jenna.mui@stonybrook.edu>
2022-09-12 00:25:36 -0400

    Update code.py
    update code

--- code.py
+++ code.py
@@ -6,25 +6,31 @@ import tabulate
 patients = pd.read_csv('data/Hospital_Inpatient_Discharges__SPARCS_De-Identified___2015.csv')
 neighborhood = pd.read_csv('data/NY_2015_ADI_9 Digit Zip Code_v3.1.csv')
 
+#list columns
 list(patients)
 list(neighborhood)
 
+#Edit column names
 patients.columns = patients.columns.str.replace('[^A-Za-z0-9]+', '_')
 
+#Create smaller dataframes with specific columns
 df_patients_small = patients[['Zip_Code_3_digits','CCS_Diagnosis_Code','CCS_Diagnosis_Description','Total_Charges','Total_Costs']]
 print(df_patients_small.sample(10).to_markdown())
 list(df_patients_small)
 df_patients_small.shape
 
-df_neighborhood_small = [['ZIPID','ADI_NATRANK','ADI_STATERNK']]
+
+df_neighborhood_small = neighborhood[['ZIPID']]
 print(df_neighborhood_small.sample(10).to_markdown())
 list(df_neighborhood_small)
 df_neighborhood_small.shape 
 
+#Combine two dataframes
 combined_df = df_patients_small.merge(df_neighborhood_small, how='left', left_on='Zip_Code_3_digits', right_on='ZIPID')
 combined_df = pd.merge(df_patients_small, df_neighborhood_small, how='left', left_on='Zip_Code_3_digits', right_on='ZIPID')
 
 combined_df.columns
+
 ### save to csv 
 combined_df.to_csv('data/combined_df.csv')
 combined_df.shape
